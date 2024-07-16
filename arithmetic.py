b31af966f1ef858343318c507d31da973f7ff541
JorelCain <jorelcain2@gmail.com>
2024-07-16 12:10:38 -0700

    initial commit

---
+++ arithmetic.py
@@ -0,0 +1,7 @@
+bread = 2.75
+peanut_butter= 4.50
+jelly = 3.23
+
+total = bread + peanut_butter + jelly
+
+print(total)
---
+++ if_statements.py
@@ -0,0 +1,13 @@
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+mood = input("How are you feeling today?")
+
+if mood == "happy":
+    print("That's great to hear!")
+else:
+    print("I hope your day gets better!")
